799d8267742f04c7629f26ce83b379b5bd92278c
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-07-25 23:59:15 +0300

    done

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    // baseURL: 'http://localhost:9000/api'
+    // baseURL: 'http://localhost:9000/api/v1'
     baseURL: 'https://springhw1-f5412575bf3e.herokuapp.com/api/v1'
 });
